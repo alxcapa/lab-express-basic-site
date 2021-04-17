9b14af81cdc7b6bfde32a31a0bec04b3ee80db2b
Projetphp <alexandre.capaldi@hotmail.fr>
2021-04-17 12:06:33 +0200

    suite

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ const app = express();
 
 app.use(express.static("public"));
 
+
 app.get("/home", function (req, res, next) {
-  res.sendFile(__dirname + `/views/home-page.html`);
+    res.sendFile(__dirname + `/views/home-page.html`);
 });
 
 app.get("/about", function (req, res, next) {
-  res.sendFile(__dirname + `/views/about-page.html`);
+    res.sendFile(__dirname + `/views/about-page.html`);
 });
 
 app.get("/works", function (req, res, next) {
-  res.sendFile(__dirname + `/views/works-page.html`);
+    res.sendFile(__dirname + `/views/works-page.html`);
 });
 
 app.get("/gallery", function (req, res, next) {
-  res.sendFile(__dirname + `/views/gallery-page.html`);
+    res.sendFile(__dirname + `/views/gallery-page.html`);
 });
 
 app.listen(3000);
--- public/js/main.js
+++ public/js/main.js
@@ -4,8 +4,8 @@ crystalTheme.src = "music/crystal-theme.mp3"
 
 let $crystal = document.querySelector('.minicristal');
 
-$crystal.addEventListener("click", () =>{
-    if ($crystal.className === 'minicristal'){
+$crystal.addEventListener("click", () => {
+    if ($crystal.className === 'minicristal') {
         crystalTheme.play();
         $crystal.classList.add('on');
     } else {
