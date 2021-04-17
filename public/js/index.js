340ba586dab774618a12c55a9fbe0ac89de0e2eb
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-04-17 11:37:45 +0200

    lasuite

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+let crystalTheme = document.createElement("audio");
+crystalTheme.src = "music/crystal-theme.mp3"
+
+
+let $crystal = document.querySelector('.minicristal');
+
+$crystal.addEventListener("click", () =>{
+    if ($crystal.className === 'minicristal'){
+        crystalTheme.play();
+        $crystal.classList.add('on');
+    } else {
+        crystalTheme.pause();
+        $crystal.classList.remove('on');
+    }
+
+});
